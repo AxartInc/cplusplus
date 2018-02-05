168c0824d81a78170bca25270f8ba1439e30e5f4
Ax_art <axart@protonmail.com>
2018-02-05 18:27:24 +0100

    pas fini exo 5, une seule erreur

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -55,5 +55,98 @@ void exo3(void) {
 	}
 }
 
+unsigned short lancer_de()
+{
+	srand(time(NULL));
+	return (rand() % 6) + 1;
+}
+
+void exo4(void) {
+
+	int point = 0;
+	int joueur1 = 0;
+	int joueur2 = 0;
+	int tour = 0;
+
+	while (joueur1 >= 50 && joueur2 >= 50); {
+		int x = lancer_de();
+		std::cin.get();
+		while (x != 1) {
+			if (x % 2 == 0) {
+				point = point + x;
+					if (tour % 2 == 0) {
+						joueur2 = joueur2 + point;
+						char point;
+						std::cout << "Le joueur 2 a : "
+							<< point
+							<< std::endl;
+					} else {
+						joueur1 = joueur1 + point;
+						char point;
+						std::cout << "Le joueur 1 a : "
+							<< point
+							<< std::endl;
+					}
+			}
+			if (x = 3) {
+				point = point * 3;
+				if (tour % 2 == 0) {
+					joueur2 = joueur2 + point;
+					char point;
+					std::cout << "Le joueur 2 a : "
+						<< point
+						<< std::endl;
+				}
+				else {
+					joueur1 = joueur1 + point;
+					char point;
+					std::cout << "Le joueur 1 a : "
+						<< point
+						<< std::endl;
+				}
+			}
+			if (x = 5) {
+				point = 0;
+				if (tour % 2 == 0) {
+					joueur2 = joueur2 + point;
+					char point;
+					std::cout << "Le joueur 2 a : "
+						<< point
+						<< std::endl;
+				} else {
+					joueur1 = joueur1 + point;
+					char point;
+					std::cout << "Le joueur 1 a : "
+						<< point
+						<< std::endl;
+				}
+			}
+		} else {
+			
+			char point;
+			std::cout << "Le joueur a : "
+				<< point
+				<< std::endl;
+			char tour;
+			std::cout << "Tour numero :  "
+				<< tour
+				<< std::endl;
+			tour++;
+			point = 0;
+			x = 0;
+			std::cout << "Changement de joueur !"
+				<< std::endl;
+	}
+}
+	if (joueur1 > joueur2) {
+		std::cout << "Le joueur 1 gagnant"
+				  << std::endl;
+	}
+	else {
+		std::cout << "Le joueur 2 gagnant"
+				  << std::endl;
+	}
+}
+
 #endif
 
